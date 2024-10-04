6a908558ddbc1d4b9295eb41919fc42a134f32e6
Navya N <navyan@Navyas-MacBook-Pro-2.local>
2024-10-04 18:13:02 -0500

    rubocop fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# UsersController handles displaying user-related data.
+# The show action retrieves and displays the current user based on the ID provided in the parameters.
 class UsersController < ApplicationController
   def show
     @current_user = User.find(params[:id])
