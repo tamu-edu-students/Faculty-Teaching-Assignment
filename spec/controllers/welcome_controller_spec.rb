f4918a862517ebbc9b0f3055a0bf1aec06444afb
Navya Unnikrishnan <naviunni@gmail.com>
2024-10-04 16:35:08 -0500

    fix rubocop issues

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # spec/controllers/welcome_controller_spec.rb
 require 'rails_helper'
 
@@ -5,7 +7,8 @@ RSpec.describe WelcomeController, type: :controller do
   describe 'GET #index' do
     context 'when user is logged in' do
       before do
-        @user = User.create!( uid: '12345', provider: 'google_oauth2', email: 'test@example.com', first_name: 'John', last_name: 'Doe')
+        @user = User.create!(uid: '12345', provider: 'google_oauth2', email: 'test@example.com', first_name: 'John',
+                             last_name: 'Doe')
         allow(controller).to receive(:logged_in?).and_return(true)
         controller.instance_variable_set(:@current_user, @user)
       end
@@ -13,7 +16,7 @@ RSpec.describe WelcomeController, type: :controller do
       it 'redirects to the user path with a welcome notice' do
         get :index
         expect(response).to redirect_to(user_path(@user))
-        expect(flash[:notice]).to eq("Welcome back, John!")
+        expect(flash[:notice]).to eq('Welcome back, John!')
       end
     end
 
