a6f1daf1060bb7e64fcc2395d5bfa39847da07f3
Navya Unnikrishnan <naviunni@gmail.com>
2024-10-13 17:27:25 -0500

    Add schedules table

---
+++ db/migrate/20241013031920_create_schedules.rb
@@ -0,0 +1,10 @@
+class CreateSchedules < ActiveRecord::Migration[7.2]
+  def change
+    create_table :schedules do |t|
+      t.string :schedule_name
+      t.string :semester_name
+
+      t.timestamps
+    end
+  end
+end
