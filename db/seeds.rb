f706547c68d2d69d734f0513674c13ea78185a5f
Wahib Kapdi <wahibkapdi@tamu.edu>
2024-10-14 14:52:38 -0500

    Added Basic Data Seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'json'
+
+ROOMS_DATA_PATH = 'db/rooms.json'
+
+def seed_room_data
+    file = File.open(ROOMS_DATA_PATH).read
+    json_room_data = JSON.parse file
+    
+    puts json_room_data
+end
