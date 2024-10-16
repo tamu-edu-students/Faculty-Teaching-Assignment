a179f150744f05a6c9db57fc6eee50e664a9866e
Navya Unnikrishnan <naviunni@gmail.com>
2024-10-16 15:10:57 -0500

    Fix rubocop issues

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
-  
-    factory :schedule do
-      schedule_name {"Test Schedule"}
-      semester_name {"Fall 2024"}
-    end
+  factory :schedule do
+    schedule_name { 'Test Schedule' }
+    semester_name { 'Fall 2024' }
   end
+end
