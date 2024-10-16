31a0e27714b3743f216647ec01210f4456b31dac
pavitgopal <pavitgopal@gmail.com>
2024-10-15 22:39:05 -0500

    created time_slot.rb file

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,13 +1,5 @@
-class CreateTimeSlots < ActiveRecord::Migration[6.1]
-    def change
-      create_table :time_slots do |t|
-        t.string :day
-        t.string :start_time
-        t.string :end_time
-        t.string :slot_type # Either 'LEC' or 'LAB'
-  
-        t.timestamps
-      end
-    end
+
+class TimeSlot < ApplicationRecord
+    
   end
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,60 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+TimeSlot.create([
+  { day: 'MWF', start_time: '8:00', end_time: '8:50', slot_type: 'LEC' },
+  { day: 'MWF', start_time: '9:10', end_time: '10:00', slot_type: 'LEC' },
+  { day: 'MWF', start_time: '10:20', end_time: '11:10', slot_type: 'LEC' },
+  { day: 'MWF', start_time: '11:30', end_time: '12:20', slot_type: 'LEC' },
+  { day: 'MWF', start_time: '12:40', end_time: '13:30', slot_type: 'LEC' },
+  { day: 'MWF', start_time: '13:50', end_time: '14:20', slot_type: 'LEC' },
+  { day: 'MWF', start_time: '15:00', end_time: '15:50', slot_type: 'LEC' },
+  
+  { day: 'MW', start_time: '16:10', end_time: '17:25', slot_type: 'LEC' },
+  { day: 'MW', start_time: '17:45', end_time: '19:00', slot_type: 'LEC' },
+  
+  { day: 'TR', start_time: '8:00', end_time: '9:15', slot_type: 'LEC' },
+  { day: 'TR', start_time: '9:35', end_time: '10:50', slot_type: 'LEC' },
+  { day: 'TR', start_time: '11:10', end_time: '12:25', slot_type: 'LEC' },
+  { day: 'TR', start_time: '12:45', end_time: '14:00', slot_type: 'LEC' },
+  { day: 'TR', start_time: '14:20', end_time: '15:35', slot_type: 'LEC' },
+  { day: 'TR', start_time: '16:55', end_time: '17:10', slot_type: 'LEC' },
+  { day: 'TR', start_time: '17:30', end_time: '18:45', slot_type: 'LEC' },
+  
+  { day: 'MW', start_time: '8:00', end_time: '8:50', slot_type: 'LAB' },
+  { day: 'MW', start_time: '9:10', end_time: '10:00', slot_type: 'LAB' },
+  { day: 'MW', start_time: '10:20', end_time: '11:10', slot_type: 'LAB' },
+  { day: 'MW', start_time: '11:30', end_time: '12:20', slot_type: 'LAB' },
+  { day: 'MW', start_time: '12:40', end_time: '13:30', slot_type: 'LAB' },
+  { day: 'MW', start_time: '13:50', end_time: '14:40', slot_type: 'LAB' },
+  { day: 'MW', start_time: '15:00', end_time: '15:50', slot_type: 'LAB' },
+  { day: 'MW', start_time: '16:10', end_time: '17:00', slot_type: 'LAB' },
+  { day: 'MW', start_time: '17:45', end_time: '18:35', slot_type: 'LAB' },
+  { day: 'MW', start_time: '19:10', end_time: '19:50', slot_type: 'LAB' },
+  
+  { day: 'TR', start_time: '8:25', end_time: '9:15', slot_type: 'LAB' },
+  { day: 'TR', start_time: '9:35', end_time: '10:25', slot_type: 'LAB' },
+  { day: 'TR', start_time: '11:35', end_time: '12:25', slot_type: 'LAB' },
+  { day: 'TR', start_time: '12:45', end_time: '13:35', slot_type: 'LAB' },
+  { day: 'TR', start_time: '14:35', end_time: '15:25', slot_type: 'LAB' },
+  { day: 'TR', start_time: '15:55', end_time: '16:45', slot_type: 'LAB' },
+  { day: 'TR', start_time: '17:30', end_time: '18:20', slot_type: 'LAB' },
+  { day: 'TR', start_time: '18:40', end_time: '19:30', slot_type: 'LAB' },
+
+  { day: 'F', start_time: '8:00', end_time: '9:40', slot_type: 'LAB' },
+  { day: 'F', start_time: '10:20', end_time: '12:00', slot_type: 'LAB' },
+  { day: 'F', start_time: '12:40', end_time: '14:20', slot_type: 'LAB' },
+
+  { day: 'F', start_time: '8:00', end_time: '8:50', slot_type: 'LAB' },
+  { day: 'F', start_time: '9:10', end_time: '10:00', slot_type: 'LAB' },
+  { day: 'F', start_time: '10:20', end_time: '11:10', slot_type: 'LAB' },
+  { day: 'F', start_time: '11:30', end_time: '12:20', slot_type: 'LAB' },
+  { day: 'F', start_time: '12:40', end_time: '13:30', slot_type: 'LAB' },
+  { day: 'F', start_time: '13:50', end_time: '14:40', slot_type: 'LAB' },
+  { day: 'F', start_time: '15:00', end_time: '15:50', slot_type: 'LAB' },
+
+
+  # Add all other time slots here
+])
+
