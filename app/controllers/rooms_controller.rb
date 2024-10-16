2b53733da656992da522ca417fc633e1c44282b1
Wahib Kapdi <wahibkapdi@tamu.edu>
2024-10-16 13:53:33 -0500

    Rooms View

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,9 @@
 class RoomsController < ApplicationController
   def index
-    @rooms = Room.where(is_active: true)
+    if params[:active_rooms] == "true"
+      @rooms = Room.where(is_active: true)
+    else
+      @rooms = Room.all
+    end
   end
 end
--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,21 @@
 module RoomsHelper
+    def display_usage_type (value, is_active)
+        classes = "bg-secondary"
+        message = "NA"
+        extra_classes = ""
+        if is_active
+            extra_classes = " fw-bold text-white"
+        end
+        if value
+            classes = "bg-success"
+            message = "Yes"
+        elsif is_active
+            classes = "bg-danger"
+            message = "No"
+        else
+            classes = "bg-secondary"
+        end
+        classes += extra_classes
+        render partial: "/rooms/usage_type", locals: {classes: classes, message: message}
+    end
 end
