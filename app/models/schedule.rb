dbfe2cf1ba7ab3c0dc3cd9917060823badf68717
Navya Unnikrishnan <naviunni@gmail.com>
2024-10-16 15:49:49 -0500

    Add top-level documentation for files

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# The model for the Schedule class represents the schedules
 class Schedule < ApplicationRecord
   validates :schedule_name, :semester_name, presence: true
 end
