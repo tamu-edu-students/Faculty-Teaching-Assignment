6d694b54a0347630622b8f4d06493104fc2c7c6d
Wahib Kapdi <wahibkapdi@tamu.edu>
2024-10-17 05:48:06 -0500

    Sorting and Filtering Added

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,26 @@
 class RoomsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+  
   def index
     if params[:active_rooms] == "true"
       @rooms = Room.where(is_active: true)
+      @active_filter = true
     else
       @rooms = Room.all
+      @active_filter = false
     end
+
+    @rooms = @rooms.order("#{sort_column} #{sort_direction}")
+  end
+
+  private
+  # Define the allowed sorting columns
+  def sort_column
+    Room.column_names.include?(params[:sort]) ? params[:sort] : "building_code"
+  end
+
+  # Define the sorting direction (ascending or descending)
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -18,4 +18,11 @@ module RoomsHelper
         classes += extra_classes
         render partial: "/rooms/usage_type", locals: {classes: classes, message: message}
     end
+
+    def sortable(column, title = nil)
+        title ||= column.titleize
+        direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+        classes = (direction == "asc") ? "sortable-link" : "sortable-link desc";
+        link_to title, { sort: column, direction: direction, active: params[:active_rooms] }, class: classes
+    end
 end
