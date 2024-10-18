8490b205ae1bf49edb0bac70c83c8369f4034ac1
pavitgopal <pavitgopal@gmail.com>
2024-10-18 09:56:12 -0500

    added filters for sorting

--- app/controllers/time_slots_controller.rb
+++ app/controllers/time_slots_controller.rb
@@ -1,5 +1,7 @@
 class TimeSlotsController < ApplicationController
   def index
     @time_slots = TimeSlot.all
+    @time_slots = @time_slots.where(day: params[:day]) if params[:day].present?
+    @time_slots = @time_slots.where(slot_type: params[:slot_type]) if params[:slot_type].present?
   end
 end
