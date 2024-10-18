cdb0b823c671bb5c55660d794057892f3065cc74
pavitgopal <pavitgopal@gmail.com>
2024-10-18 15:43:48 -0500

    fixed dark styling

--- app/controllers/time_slots_controller.rb
+++ app/controllers/time_slots_controller.rb
@@ -6,6 +6,6 @@ class TimeSlotsController < ApplicationController
     @time_slots = @time_slots.where(day: params[:day]) if params[:day].present?
     @time_slots = @time_slots.where(slot_type: params[:slot_type]) if params[:slot_type].present?
 
-    @schedule = Schedule.find(params[:schedule_id])
+    @schedule = (Schedule.find(params[:schedule_id]) if params[:schedule_id].present?)
   end
 end
