f6227331d8d736b6bef458dda94cc0f93f6cbe78
pavitgopal <pavitgopal@gmail.com>
2024-10-18 11:09:46 -0500

    added time slot validation

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,4 +1,16 @@
-# frozen_string_literal: true
-
 class TimeSlot < ApplicationRecord
-end
+    validates :day, presence: true
+    validates :start_time, presence: true
+    validates :end_time, presence: true
+    validates :slot_type, presence: true
+    validate :end_time_after_start_time
+  
+    private
+  
+    def end_time_after_start_time
+      if end_time.present? && start_time.present? && end_time <= start_time
+        errors.add(:end_time, "must be after the start time")
+      end
+    end
+  end
+  
