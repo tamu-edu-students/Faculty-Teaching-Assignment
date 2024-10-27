1a5461bc97d83f4d69e31a044f0b66d1f7aef3c3
Wahib Kapdi <wahibkapdi@tamu.edu>
2024-10-26 22:28:50 -0500

    Basic View Added

--- app/controllers/room_bookings_controller.rb
+++ app/controllers/room_bookings_controller.rb
@@ -3,12 +3,18 @@ class RoomBookingsController < ApplicationController
         schedule_id = params[:schedule_id]
         @schedule = Schedule.find(params[:schedule_id])
         @rooms = @schedule.rooms
-        @time_slots = TimeSlot.all
         @tabs = TimeSlot.distinct.pluck(:day)
         @active_tab = params[:active_tab] || @tabs[0]
+        @time_slots = TimeSlot.where(time_slots: {day: @active_tab})
+        
         # Fetch room bookings only for the specified schedule
         @room_bookings = RoomBooking.joins(:room, :time_slot)
-                                    .where(rooms: { schedule_id: schedule_id })
+                                    .where(rooms: { schedule_id: schedule_id }, time_slots: {day: @active_tab})
+
+        # Organize room bookings in a hash with room_id and time_slot_id as keys
+        @bookings_matrix = @room_bookings.each_with_object({}) do |booking, hash|
+            hash[[booking.room_id, booking.time_slot_id]] = booking
+        end
     end
 end
   
