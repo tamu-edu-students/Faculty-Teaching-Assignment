593fbbe870e19b6990f26558736610755bd95879
Wahib Kapdi <wahibkapdi@tamu.edu>
2024-10-26 22:02:24 -0500

    Added navbar based on unique tabs available

--- app/controllers/room_bookings_controller.rb
+++ app/controllers/room_bookings_controller.rb
@@ -4,7 +4,8 @@ class RoomBookingsController < ApplicationController
         @schedule = Schedule.find(params[:schedule_id])
         @rooms = @schedule.rooms
         @time_slots = TimeSlot.all
-
+        @tabs = TimeSlot.distinct.pluck(:day)
+        @active_tab = params[:active_tab] || @tabs[0]
         # Fetch room bookings only for the specified schedule
         @room_bookings = RoomBooking.joins(:room, :time_slot)
                                     .where(rooms: { schedule_id: schedule_id })
