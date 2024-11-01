b0ac9ccce3e8cd4c278ccb71ed93b0235ddd0acc
BenFan1002 <ben109478702@gmail.com>
2024-10-31 21:15:03 -0500

    Add course and instructor id to room booking table

---
+++ db/migrate/20241101020557_add_course_and_instructor_to_room_bookings.rb
@@ -0,0 +1,6 @@
+class AddCourseAndInstructorToRoomBookings < ActiveRecord::Migration[7.2]
+  def change
+    add_reference :room_bookings, :course, null: false, foreign_key: true
+    add_reference :room_bookings, :instructor, null: false, foreign_key: true
+  end
+end
