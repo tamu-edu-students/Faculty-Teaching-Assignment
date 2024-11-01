f1e4c2c3d35fb64e36df5343bd35c5429f9ed287
BenFan1002 <ben109478702@gmail.com>
2024-11-01 16:18:47 -0500

    Fixed Roombooking test with new added foreign key instructor and course

--- spec/controllers/room_bookings_controller_spec.rb
+++ spec/controllers/room_bookings_controller_spec.rb
@@ -9,8 +9,10 @@ RSpec.describe RoomBookingsController, type: :controller do
   let!(:room2) { create(:room, schedule:) }
   let!(:time_slot1) { create(:time_slot, day: 'Monday', start_time: '09:00', end_time: '10:00') }
   let!(:time_slot2) { create(:time_slot, day: 'Monday', start_time: '10:00', end_time: '11:00') }
-  let!(:room_booking1) { create(:room_booking, room: room1, time_slot: time_slot1, is_available: true) }
-  let!(:room_booking2) { create(:room_booking, room: room2, time_slot: time_slot2, is_available: false) }
+  let!(:course) { create(:course, schedule: schedule) }
+  let!(:instructor) { create(:instructor, schedule: schedule) }
+  let!(:room_booking1) { create(:room_booking, room: room1, time_slot: time_slot1, is_available: true, course:course, instructor:instructor ) }
+  let!(:room_booking2) { create(:room_booking, room: room2, time_slot: time_slot2, is_available: false, course:course, instructor:instructor ) }
 
   before do
     @user = User.create!(uid: '12345', provider: 'google_oauth2', email: 'test@example.com', first_name: 'John',
